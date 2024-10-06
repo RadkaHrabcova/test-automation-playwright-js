92d9ab2f5a6e4ccd385b3e630016591cd48871b0
RadkaHrabcova <radkahrabcova@gmail.com>
2024-10-06 16:31:27 +0200

    fix of package-lock.json and package.json, homework

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -2,5 +2,7 @@ import { test } from "@playwright/test";
 
 test("should open login page", async ({ page }) => {
     await page.goto("/prihlaseni");
-    console.log(await page.title());
+    console.log("This is my first test!");
+    await page.setViewportSize({ width: 1920, height: 1080 });
+    await page.screenshot({ path: "login_page_1920_1080.png" });
 });
