af676b5eac193b8a5f2930c6481729495cbbf151
RadkaHrabcova <radkahrabcova@gmail.com>
2024-10-10 13:18:16 +0200

    homework selectors

--- src/tests/homework.spec.js
+++ src/tests/homework.spec.js
@@ -4,5 +4,29 @@ test("should make registration page screenshot", async ({ page }) => {
     await page.goto("/registrace");
     await page.setViewportSize({ width: 1920, height: 1080 });
     await page.screenshot({ path: "registration_page_1920_1080.png" });
+});
 
+test("should screenshot registration name input ", async ({ page }) => {
+    await page.goto("/registrace");
+    await page.locator("#name").screenshot({ path: "css_registration_input_name.png" });
+});
+
+test("should screenshot registration email input ", async ({ page }) => {
+    await page.goto("/registrace");
+    await page.locator("#email").screenshot({ path: "css_registration_input_email.png" });
+});
+
+test("should screenshot registration password input ", async ({ page }) => {
+    await page.goto("/registrace");
+    await page.locator("#password-confirm").screenshot({ path: "css_registration_input_password.png" });
+});
+
+test("should screenshot registration password confirm input ", async ({ page }) => {
+    await page.goto("/registrace");
+    await page.locator("#name").screenshot({ path: "css_registration_input_password_confirm.png" });
+});
+
+test("should screenshot registration button", async ({ page }) => {
+    await page.goto("/registrace");
+    await page.locator(".btn-primary").screenshot({ path: "css_registration_btn_primary.png" });
 });
